7d8fbce0d159908e76bbf6aa634226b09ff4cefc
manasdeo <rmnslogin@gmail.com>
2024-10-06 12:41:42 +0530

    Shadow

--- guess-my-number/App.tsx
+++ guess-my-number/App.tsx
@@ -1,13 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <View>
-      <StartGameScreen />
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
 const styles = StyleSheet.create({});
