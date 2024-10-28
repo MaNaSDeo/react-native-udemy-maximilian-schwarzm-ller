457c06888908d208a4cb25155613bb16322f063a
manasdeo <rmnslogin@gmail.com>
2024-10-28 22:47:54 +0530

    Drawer navigation

---
+++ todo-app/App.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./Screens/WelcomeScreen";
+import UserScreen from "./Screens/UserScreen";
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="User">
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ todo-app/Screens/WelcomeScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const WelcomeScreen = () => {
+  return <Text>WelcomeScreen</Text>;
+};
+
+export default WelcomeScreen;
